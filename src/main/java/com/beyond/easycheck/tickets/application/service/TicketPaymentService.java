589187dd4421f80332ef6ffe34b23e1cd43e5b15
Jihoon Lim <bo9701@naver.com>
2024-10-31 02:50:03 +0900

    feat: 무통장 입금 기능 로직 작성

--- src/main/java/com/beyond/easycheck/tickets/application/service/TicketPaymentService.java
+++ src/main/java/com/beyond/easycheck/tickets/application/service/TicketPaymentService.java
@@ -2,6 +2,9 @@ package com.beyond.easycheck.tickets.application.service;
 
 import com.beyond.easycheck.common.exception.EasyCheckException;
 import com.beyond.easycheck.mail.application.service.MailService;
+import com.beyond.easycheck.payments.exception.PaymentMessageType;
+import com.beyond.easycheck.payments.infrastructure.entity.CompletionStatus;
+import com.beyond.easycheck.payments.infrastructure.entity.PaymentEntity;
 import com.beyond.easycheck.tickets.exception.TicketOrderMessageType;
 import com.beyond.easycheck.tickets.infrastructure.entity.OrderStatus;
 import com.beyond.easycheck.tickets.infrastructure.entity.PaymentStatus;
@@ -70,64 +73,72 @@ public class TicketPaymentService {
         IamportResponse<Payment> paymentResponse = validatePortOnePayment(request.getImpUid());
 
         if (paymentResponse != null && paymentResponse.getResponse().getAmount().compareTo(request.getPaymentAmount()) == 0) {
-            TicketPaymentEntity result = createAndCompletePayment(order, request);
+            if ("vbank".equals(request.getPaymentMethod())) {
+                handleVirtualAccountTicketPayment(request, order, paymentResponse);
+            } else {
+                TicketPaymentEntity result = createAndCompletePayment(request, order);
 
-            // Construct the view for the email content
-            TicketPaymentView ticketPaymentView = new TicketPaymentView(
-                    result.getId(),
-                    result.getImpUid(),
-                    result.getTicketOrder().getId(),
-                    result.getTicketOrder().getUserEntity().getId(),
-                    result.getTicketOrder().getUserEntity().getName(),
-                    result.getTicketOrder().getUserEntity().getPoint(),
-                    result.getTicketOrder().getTicket().getThemePark().getAccommodation().getName(),
-                    result.getTicketOrder().getTicket().getTicketName(),
-                    result.getTicketOrder().getTicket().getThemePark().getName(),
-                    result.getTicketOrder().getTicket().getValidFromDate(),
-                    result.getTicketOrder().getTicket().getValidToDate(),
-                    result.getTicketOrder().getQuantity(),
-                    result.getPaymentStatus(),
-                    result.getCancelDate(),
-                    result.getPaymentMethod(),
-                    result.getPaymentAmount(),
-                    result.getPaymentDate()
-            );
+                TicketPaymentView ticketPaymentView = new TicketPaymentView(
+                        result.getId(),
+                        result.getImpUid(),
+                        result.getTicketOrder().getId(),
+                        result.getTicketOrder().getUserEntity().getId(),
+                        result.getTicketOrder().getUserEntity().getName(),
+                        result.getTicketOrder().getUserEntity().getPoint(),
+                        result.getTicketOrder().getTicket().getThemePark().getAccommodation().getName(),
+                        result.getTicketOrder().getTicket().getTicketName(),
+                        result.getTicketOrder().getTicket().getThemePark().getName(),
+                        result.getTicketOrder().getTicket().getValidFromDate(),
+                        result.getTicketOrder().getTicket().getValidToDate(),
+                        result.getTicketOrder().getQuantity(),
+                        result.getPaymentStatus(),
+                        result.getCancelDate(),
+                        result.getPaymentMethod(),
+                        result.getPaymentAmount(),
+                        result.getPaymentDate()
+                );
 
-            BigDecimal paymentAmount = request.getPaymentAmount();
-            BigDecimal pointsPercentage = new BigDecimal("0.04");
+                BigDecimal paymentAmount = request.getPaymentAmount();
+                BigDecimal pointsPercentage = new BigDecimal("0.04");
 
-            int pointsToAccumulate = paymentAmount.multiply(pointsPercentage)
-                    .setScale(0, RoundingMode.FLOOR)
-                    .intValue();
-            userService.accumulatePoints(pointsToAccumulate);
+                int pointsToAccumulate = paymentAmount.multiply(pointsPercentage)
+                        .setScale(0, RoundingMode.FLOOR)
+                        .intValue();
+                userService.accumulatePoints(pointsToAccumulate);
 
-            mailService.sendTicketPaymentConfirmationEmail(result.getTicketOrder().getUserEntity().getEmail(), ticketPaymentView);
+                mailService.sendTicketPaymentConfirmationEmail(result.getTicketOrder().getUserEntity().getEmail(), ticketPaymentView);
 
-            return ticketPaymentView;
-        } else {
-            throw new EasyCheckException(TicketOrderMessageType.PORTONE_VERIFICATION_ERROR);
+                return ticketPaymentView;
+            }
         }
+
+        throw new EasyCheckException(TicketOrderMessageType.PORTONE_VERIFICATION_ERROR);
     }
 
-    private TicketPaymentEntity createAndCompletePayment(TicketOrderEntity order, TicketPaymentRequest request) {
-        log.info("[createAndCompletePayment] - {}", request);
-        TicketPaymentEntity payment = new TicketPaymentEntity(order, request.getImpUid(), request.getPaymentAmount(), request.getPaymentMethod());
-        log.info("[createAndCompletePayment] - {}", request);
-        try {
-            payment.completePayment();
-            ticketPaymentRepository.save(payment);
+    public void handleVirtualAccountTicketPayment(TicketPaymentRequest request, TicketOrderEntity order, IamportResponse<Payment> paymentResponse) {
 
-            order.completeOrder();
-            ticketOrderRepository.save(order);
+        log.info("가상계좌 결제 처리: 은행 = {}, 계좌명 = {}", request.getBank(), request.getAccountHolder());
 
-            log.info("주문 ID: {} 결제 성공, 결제 금액: {}", order.getId(), request.getPaymentAmount());
-        } catch (Exception e) {
-            payment.failPayment();
-            log.info("에러났음 근데 왜 저장? {}", payment);
-            ticketPaymentRepository.save(payment);
-            handlePaymentException(e, order);
-        }
-        return payment;
+        TicketPaymentEntity ticketPaymentEntity = createAndCompletePayment(request, order);
+        ticketPaymentEntity.updatePaymentStatus(PaymentStatus.PENDING);
+
+        log.info("가상계좌 생성됨: {} - {} 은행, 입금자: {}", request.getBank(), request.getPaymentAmount(), request.getAccountHolder());
+        ticketPaymentRepository.save(ticketPaymentEntity);
+    }
+
+    @Transactional
+    public TicketPaymentEntity createAndCompletePayment(TicketPaymentRequest ticketPaymentRequest, TicketOrderEntity ticketOrderEntity) {
+
+        return TicketPaymentEntity.builder()
+                .ticketOrder(ticketOrderEntity)
+                .impUid(ticketPaymentRequest.getImpUid())
+                .paymentAmount(ticketPaymentRequest.getPaymentAmount())
+                .paymentMethod(ticketPaymentRequest.getPaymentMethod())
+                .bank(ticketPaymentRequest.getBank())
+                .accountHolder(ticketPaymentRequest.getAccountHolder())
+                .depositDeadline(ticketPaymentRequest.getDepositDeadline())
+                .paymentStatus(ticketPaymentRequest.getPaymentStatus() != null ? ticketPaymentRequest.getPaymentStatus() : PaymentStatus.PENDING)
+                .build();
     }
 
     public IamportResponse<Payment> validatePortOnePayment(String impUid) {
