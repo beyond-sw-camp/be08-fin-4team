fcc83a2a494e0eb51870628cd4507794a92805f7
yongun2 <djkye0001@naver.com>
2024-11-05 14:11:29 +0900

    chore: buildspec 수정

--- src/main/java/com/beyond/easycheck/common/security/config/EasyCheckSecurityConfig.java
+++ src/main/java/com/beyond/easycheck/common/security/config/EasyCheckSecurityConfig.java
@@ -117,7 +117,13 @@ public class EasyCheckSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:8081", "http://localhost:3000", "https://be08-fin-easy-check-easy-stay-front-client.vercel.app", "https://be08-fin-easy-check-easy-stay-front-admin.vercel.app"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080",
+                        "http://localhost:8081",
+                        "http://localhost:3000",
+                        "https://be08-fin-easy-check-easy-stay-front-client.vercel.app",
+                        "https://be08-fin-easy-check-easy-stay-front-admin.vercel.app"
+                )
+        );
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
