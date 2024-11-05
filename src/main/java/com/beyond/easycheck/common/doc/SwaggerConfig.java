9fecc9a20b3db56c108ef993ee7fdc9e4ae8f1d2
yongun2 <djkye0001@naver.com>
2024-11-05 16:59:10 +0900

    chore: 배포 확인용

--- src/main/java/com/beyond/easycheck/common/doc/SwaggerConfig.java
+++ src/main/java/com/beyond/easycheck/common/doc/SwaggerConfig.java
@@ -14,7 +14,7 @@ import java.util.Collections;
 @OpenAPIDefinition(
         info = @Info(title = "EasyCheck Service API",
                 description = "이지 체크 API",
-                version = "0.2"))
+                version = "0.3"))
 @Configuration
 public class SwaggerConfig {
     @Bean
